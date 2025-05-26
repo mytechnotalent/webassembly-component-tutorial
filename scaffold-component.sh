6210f94451608ba5627d8899e4e84873c661791d
Kevin Thomas <kevin@mytechnotalent.com>
2025-05-26 16:13:52 -0400

    Update scaffold-component.sh

--- scaffold-component.sh
+++ scaffold-component.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cargo install --locked cargo-component
+cargo install --locked wasm-tools
+curl https://wasmtime.dev/install.sh -sSf | bash
 cargo component new hello-world --lib
 cd hello-world
 cargo component build --release
