2cb69fe173bd8e3993cb974610423dac9b090836
Kevin Thomas <kevin@mytechnotalent.com>
2025-10-16 11:03:26 -0400

    Init commit

--- scaffold-component.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cargo install --locked cargo-component
-cargo install --locked wasm-tools
-curl https://wasmtime.dev/install.sh -sSf | bash
-cargo component new hello-world --lib
-cd hello-world
-cargo component build --release
-wasmtime run --invoke 'hello-world()' ./target/wasm32-wasip1/release/hello_world.wasm
